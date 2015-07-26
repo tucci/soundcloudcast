03563e323ae377997e54e82bcf5a0a31576513a8
steventucci <tuccisteven@gmail.com>
2015-07-26 10:55:44 -0400

    Fixes issue where the instructions page keeps showing.

--- app/background/background.js
+++ app/background/background.js
@@ -1,4 +1,10 @@
-chrome.runtime.onInstalled.addListener(function() {
-	var instructions = chrome.extension.getURL('app/instructions/instructions.html');
-	chrome.tabs.create({'url': instructions});
-});
+(function() {
+    'use strict';
+	var storage = JSON.parse(localStorage.getItem('soundcloudcast'));
+	storage = storage || {};
+	if (!storage.hasShownInstructions) {
+		var instructions = chrome.extension.getURL('app/instructions/instructions.html');
+		chrome.tabs.create({'url': instructions});
+		localStorage.setItem('soundcloudcast', JSON.stringify({hasShownInstructions: true}));
+ 	}
+}());
